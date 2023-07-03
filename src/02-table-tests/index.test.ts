7ae876337f622c1cdb0dfe4da08d75e34a29c67f
Nozeil <nozeilwrk@gmail.com>
2023-07-03 19:59:29 +0300

    feat: implement table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,39 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 2, b: 2, action: Action.Divide, expected: 1 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 3, action: 'Invalid action', expected: null },
+  { a: '2', b: '3', action: Action.Exponentiate, expected: null },
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+describe.each(testCases)('simpleCalculator', ({ a, b, action, expected }) => {
+  test('should add two numbers', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
+  test('should subtract two numbers', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
+  test('should multiply two numbers', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
+  test('should divide two numbers', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
+  test('should return null for invalid action', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
+  test('should return null for invalid arguments', () => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
 });
